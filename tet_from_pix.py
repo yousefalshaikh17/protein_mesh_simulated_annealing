b14d7796f65ecc77f88b8e6551a459046fc21692
Molly Gravett <mollygravett@PHY-MAC-211139.local>
2021-10-18 10:46:49 +0100

    uploading correct python

--- tet_from_pix.py
+++ tet_from_pix.py
@@ -5,92 +5,227 @@ Created on Thu Jul 22 11:04:41 2021
 
 @author: mollygravett
 """
-
-#print(mesh.cells[0])
+#Generating tetrahedral meshes from pixel data
 
 import numpy as np
 import vtk
-import math
-from vtk.util import numpy_support
+import mrcfile
+import vtk.util.numpy_support
+import datetime
+#import vtkwmtk from vmtk
+# from chimerax.map_data import mrc
+# ## #coarsen
+# g = mrc.open('map_equilibrium_mystructrigor_15A_0p00202.mrc')[0]
 
-file = 'clipped_fewcubes_map_thresholded_0p00202.vtk'
 
-reader = vtk.vtkDataSetReader()
-reader.SetFileName(file)
-reader.Update()
-vtk_data =  reader.GetOutput()
-points = np.array(reader.GetOutput().GetPoints().GetData())
+#to coarsen a mesh (to 15 Å for example) in chimerax use:
+#vol resample #1 spacing 15 - this coarsens to 15 Å voxels
+#save newmap.mrc model #2 - this saves your new coarsened model as "newmap.mrc"
 
-cells = np.array(reader.GetOutput().GetCells().GetData())
-print(np.size(cells))
-#cells_ = np.array([[4, 0, 1, 2, 3], [4, 0, 1, 2, 3]])
+mrcfilename = 'newmap_15A_0p00878.mrc'
+threshold = 0.00878
+chosen_filename = 'new_0p00878_mesh_15A'
 
-x = int((np.size(cells))/5)
-new_cells=np.resize(cells,(x,5))
+mrc = mrcfile.open(mrcfilename, mode='r+')
 
-#print(new_cells[0][4])
+a = mrc.data
 
-# longest_edge_array=np.zeros((x,))
-# surface_area_array=np.zeros((x,))
+nx = mrc.header.nx
+ny = mrc.header.ny
+nz = mrc.header.nz
 
-# for i in range(np.shape(new_cells)[0]):
-#     tet=new_cells[i]
+nvoxel = 0
+
+#find each point
+for z in range(0, nz):
+    for y in range(0, ny):
+        for x in range(0, nx):
+            if a[z,y,x] >= threshold:
+                nvoxel=nvoxel+1
+                
+coords = np.zeros((nvoxel*8, 3))
+ncoord = 0
+res = float(mrc.header.cella['x'])/nx
+x_trans = float(mrc.header.origin['x'])
+y_trans = float(mrc.header.origin['y'])
+z_trans = float(mrc.header.origin['z'])
+alternate = np.zeros((nvoxel,))
+location = 0
+
+
+                
+#create hex points
+for z in range(0, nz):
+    for y in range(0, ny):
+        for x in range(0, nx):
+            if a[z,y,x] >= threshold:
+                coord1 = [(x*res)+x_trans, (y*res)+y_trans, (z*res)+z_trans]
+                coords[ncoord] = coord1
+                coord2 = [((x+1)*res)+x_trans, (y*res)+y_trans, (z*res)+z_trans]
+                coords[ncoord+1] = coord2
+                coord3 = [((x+1)*res)+x_trans, ((y+1)*res)+y_trans, (z*res)+z_trans]
+                coords[ncoord+2] = coord3
+                coord4 = [(x*res)+x_trans, ((y+1)*res)+y_trans, (z*res)+z_trans]
+                coords[ncoord+3] = coord4
+                coord5 = [(x*res)+x_trans, (y*res)+y_trans, ((z+1)*res)+z_trans]
+                coords[ncoord+4] = coord5
+                coord6 = [((x+1)*res)+x_trans, (y*res)+y_trans, ((z+1)*res)+z_trans]
+                coords[ncoord+5] = coord6
+                coord7 = [((x+1)*res)+x_trans, ((y+1)*res)+y_trans, ((z+1)*res)+z_trans]
+                coords[ncoord+6] = coord7
+                coord8 = [(x*res)+x_trans, ((y+1)*res)+y_trans, ((z+1)*res)+z_trans]
+                coords[ncoord+7] = coord8
+                ncoord=ncoord+8
+                #logic for alternating tet division 0 (a) or 1 (b)
+                if z % 2 == 0:
+                    if y % 2 == 0:
+                        if x % 2 == 0:
+                            alternate[location] = 0
+                        else:
+                            alternate[location] = 1
+                    else:
+                        if x % 2 == 0:
+                            alternate[location] = 1
+                        else:
+                            alternate[location] = 0
+                else:
+                    if y % 2 == 0:
+                        if x % 2 == 0:
+                            alternate[location] = 1
+                        else:
+                            alternate[location] = 0
+                    else:
+                        if x % 2 == 0:
+                            alternate[location] = 0
+                        else:
+                            alternate[location] = 1
+                location=location+1
+                
+
     
-#     #locate points
-#     point0 = points[tet[1]]
-#     point1 = points[tet[2]]
-#     point2 = points[tet[3]]
-#     point3 = points[tet[4]]
-    
-#     #calculate distances
-#     distance_p0_p1 = math.sqrt(np.square(point0-point1).sum(axis=0))
-#     distance_p1_p2 = math.sqrt(np.square(point1-point2).sum(axis=0))
-#     distance_p2_p0 = math.sqrt(np.square(point2-point0).sum(axis=0))
-#     distance_p2_p3 = math.sqrt(np.square(point2-point3).sum(axis=0))
-#     distance_p3_p1 = math.sqrt(np.square(point3-point1).sum(axis=0))
-#     distance_p3_p0 = math.sqrt(np.square(point3-point0).sum(axis=0))
-    
-#     #triangles sorted in descending order of side length
-#     base = -np.sort(-np.array([distance_p0_p1, distance_p1_p2, distance_p2_p0]))
-#     right = -np.sort(-np.array([distance_p0_p1, distance_p3_p1, distance_p3_p0]))
-#     left = -np.sort(-np.array([distance_p3_p0, distance_p2_p3, distance_p2_p0]))
-#     back = -np.sort(-np.array([distance_p3_p1, distance_p1_p2, distance_p2_p3]))
-#     longest_edge=-np.sort(-np.concatenate((base, right, left, back)))[0] #longest edge in tet
-#     longest_edge_array[i]=longest_edge
-    
-#     #surface area
-#     base_area = 0
-#     right_area = 0
-#     left_area = 0
-#     back_area = 0
-#     #dictionary = {'base_': base, ''}
-#     for triangle in [base,right,left,back]:
-#         area = (1/4)*math.sqrt((triangle[0] + (triangle[1] + triangle[2]))*(triangle[2] - (triangle[0] - triangle[1]))*(triangle[2] + (triangle[0] - triangle[1]))*(triangle[0] + (triangle[1] - triangle[2])))
-#        # dictionary(triangle)+'area'.append()
-#         if triangle.all() == base.all():
-#             base_area=base_area+area
-#         if triangle.all() == right.all():
-#             right_area=right_area+area
-#         if triangle.all() == left.all():
-#             left_area=left_area+area
-#         if triangle.all() == back.all():
-#             back_area=back_area+area
-#     surface_area = base_area + right_area + left_area + back_area
-#     surface_area_array[i]=surface_area
-# #print(surface_area_array)
+points_ = np.unique(coords, axis=0) #make unique list of points for index
 
-# #writing to vtk
-# surface_area_array=numpy_support.numpy_to_vtk(surface_area_array)
-# surface_area_array.SetName("SURFACE_AREA")
-# vtk_data.GetCellData().AddArray(surface_area_array)
-# longest_edge_array=numpy_support.numpy_to_vtk(longest_edge_array)
-# longest_edge_array.SetName("LONGEST_EDGE")
-# vtk_data.GetCellData().AddArray(longest_edge_array)
-# writer = vtk.vtkUnstructuredGridWriter()
-# writer.SetFileName("Output.vtk")
-# writer.SetInputData(vtk_data)
-# writer.Update()
-# writer.Write()
+#create connectivity of cubes using index from point list
+connectivity_ = np.zeros((nvoxel*8,), dtype='int16')
+for pos in range(len(coords)):
+    point = coords[pos]
+    connectivity_[pos] = np.where((points_==point).all(axis=1))[0]
+cells_ = np.resize(connectivity_, (nvoxel,8))
+
+cells_con = vtk.vtkCellArray() #create vtk cell array
+tet_array = np.zeros((nvoxel*5,4), dtype='int16') #tet array for .ele
+
+#tet division for even cubes
+def a(cube):
+    tet1 = np.array([cube[0], cube[4], cube[5], cube[7]])
+    tet2 = np.array([cube[0], cube[1], cube[2], cube[5]])
+    tet3 = np.array([cube[2], cube[5], cube[6], cube[7]])
+    tet4 = np.array([cube[0], cube[2], cube[3], cube[7]])
+    tet5 = np.array([cube[0], cube[2], cube[5], cube[7]])
+    tet_list = [tet1, tet2, tet3, tet4, tet5]
+    return tet_list
+
+#tet division for odd cubes    
+def b(cube):
+    tet1 = np.array([cube[0], cube[1], cube[3], cube[4]])
+    tet2 = np.array([cube[1], cube[4], cube[5], cube[6]])
+    tet3 = np.array([cube[1], cube[2], cube[3], cube[6]])
+    tet4 = np.array([cube[3], cube[4], cube[6], cube[7]])
+    tet5 = np.array([cube[1], cube[3], cube[4], cube[6]])
+    tet_list = [tet1, tet2, tet3, tet4, tet5]
+    return tet_list
+
+#iterate over cubes and convert to tets
+for i in range(len(cells_)):
+    hex=cells_[i]
+    cube = hex[0:8]
+    if alternate[i] == 0:
+        connectivity = a(cube)
+    elif alternate[i] == 1:
+        connectivity = b(cube)
+    for tet in range(len(connectivity)):
+        tet_array[(i*5)+tet] = connectivity[tet]
+        tet_con = connectivity[tet]
+        tetra = vtk.vtkTetra()
+        tetra.GetPointIds().SetId(0, tet_con[0])
+        tetra.GetPointIds().SetId(1, tet_con[1])
+        tetra.GetPointIds().SetId(2, tet_con[2])
+        tetra.GetPointIds().SetId(3, tet_con[3])
+        cells_con.InsertNextCell(tetra) #add tet data to vtk cell array
 
 
-    
+vtkPts = vtk.vtkPoints()
+vtkPts.SetData(vtk.util.numpy_support.numpy_to_vtk(points_, deep=True))
+grid = vtk.vtkUnstructuredGrid() #create unstructured grid
+grid.SetPoints(vtkPts) #assign points to grid
+grid.SetCells(vtk.VTK_TETRA, cells_con) #assign tet cells to grid
+
+surfFilt2 = vtk.vtkDataSetSurfaceFilter()
+surfFilt2.SetInputData(grid)
+surfFilt2.Update()
+surf = surfFilt2.GetOutput()
+surf_points=np.array(surf.GetPoints().GetData())
+cells = surf.GetPolys()
+nCells = cells.GetNumberOfCells()
+array = cells.GetData()
+original_ids = np.zeros((len(surf_points),), dtype='int16')
+for pos in range(len(surf_points)):
+    point = surf_points[pos]
+    original_ids[pos] = np.where((points_==point).all(axis=1))[0]
+
+# This holds true if all polys are of the same kind, e.g. triangles.
+assert(array.GetNumberOfValues()%nCells==0)
+nCols = array.GetNumberOfValues()//nCells
+numpy_cells = np.array(array)
+faces = numpy_cells.reshape((-1,nCols))
+
+#write to vtk
+writer = vtk.vtkUnstructuredGridWriter()
+writer.SetFileName(chosen_filename+".vtk")
+writer.SetInputData(grid)
+writer.Update()
+writer.Write()
+
+#write to tetgen .ele, .node, .face
+#.ele
+#First line: <# of tetrahedra> <nodes per tetrahedron> <# of attributes>
+#Remaining lines list of # of tetrahedra:
+#<tetrahedron #> <node> <node> <node> <node> ... [attributes]
+date = datetime.datetime.now().strftime("%x")
+comment = '# created by Molly Gravett ' + date
+ele = open(chosen_filename+".1.ele", "w")
+ele_first = str(nvoxel*5)+' 4 0\n'
+ele.write(ele_first)
+for i in range(len(tet_array)):
+    ele_next=str(i+1)+' '+str(tet_array[i][0]+1)+' '+str(tet_array[i][1]+1)+' '+str(+tet_array[i][2]+1)+' '+str(tet_array[i][3]+1)+'\n'
+    ele.write(ele_next)
+ele.write(comment)
+ele.close()
+
+#.node
+#First line: <# of points> <dimension (must be 3)> <# of attributes> <# of boundary markers (0 or 1)>
+#Remaining lines list # of points:
+#<point #> <x> <y> <z>
+node = open(chosen_filename+".1.node", "w")
+node_first = str(len(points_))+' 3 0 0\n'
+node.write(node_first)
+for i in range(len(points_)):
+    node_next=str(i+1)+' '+str(points_[i][0])+' '+str(points_[i][1])+' '+str(+points_[i][2])+'\n'
+    node.write(node_next)
+node.write(comment)
+node.close()    
+
+#.face
+#First line: <# of faces> <boundary marker (0 or 1)>
+#Remaining lines list of # of faces:
+#<face #> <node> <node> <node> [boundary marker]
+
+face = open(chosen_filename+".1.face", "w")
+face_first = str(len(faces))+' 1\n'
+face.write(face_first)
+for i in range(len(faces)):
+    face_next=str(i+1)+' '+str(original_ids[faces[i][1]]+1)+' '+str(original_ids[faces[i][2]]+1)+' '+str(original_ids[faces[i][3]]+1)+' -1\n'
+    face.write(face_next)
+face.write(comment)
+face.close()
+
