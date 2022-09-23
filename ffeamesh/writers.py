dd616375244a8539967af926cbb9de7e99ffbce5
Jonathan Pickering <J.H.Pickering@leeds.ac.uk>
2022-09-23 22:55:31 +0100

    added copyright
    Former-commit-id: 2042ef4c90614215a4936d161156531bf4210179

--- ffeamesh/writers.py
+++ ffeamesh/writers.py
@@ -1,3 +1,30 @@
+"""
+ This file is part of the FFEA simulation package
+
+ Copyright (c) by the Theory and Development FFEA teams,
+ as they appear in the README.md file.
+
+ FFEA is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ FFEA is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with FFEA.  If not, see <http://www.gnu.org/licenses/>.
+
+ To help us fund FFEA development, we humbly ask that you cite
+ the research papers on the package.
+
+ Write tetrohedrons out to ffea input files, tetgen fromat.
+
+Authors: Joanna Leng, Jonathan Pickering, Molly Gravett, Jarvellis Rogers  - University of Leeds
+"""
+
 def write_ffea_output(output_file, nvoxel, tet_array, points, faces, original_ids, comment=""):
     """
     write the ffea input files
