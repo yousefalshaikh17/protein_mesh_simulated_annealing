7413caec8c370544092cb0d313bc8c58935c1f08
Jonathan Pickering <J.H.Pickering@leeds.ac.uk>
2022-10-24 23:15:21 +0100

    linted and fixed comments
    Former-commit-id: a5b3ef7cc40e271a38ce3a00f07f3611309f9356

--- ffeamesh/fivetets.py
+++ ffeamesh/fivetets.py
@@ -40,93 +40,6 @@ import ffeamesh.vtk_utility as vtk_u
 from ffeamesh import utility
 from ffeamesh.coord_utility import Coordinate
 
-def make_progress_test(end_x, end_y, end_z, steps=10, start_x=0, start_y=0, start_z=0):
-    """
-    make a progress test function
-    Args:
-        end_x (int): number of x columns
-        end_y (int): number of y columns
-        end_z (int): number of z columns
-        steps (int): the number of iterations between reporting
-        start_x (int): the count (zero start) of the first x column
-        start_y (int): the count (zero start) of the first y column
-        start_z (int): the count (zero start) of the first z column
-    Returns:
-        function(int, int, int) =>int or None
-        (int): the total number of iterations
-    """
-    total = (end_x - start_x) * (end_y - start_y) * (end_z - start_z)
-    stage = int(total/steps)
-
-    def progress_test(current_iteration):
-        """
-        test if the current iteration should be reported for prograss
-        Args:
-            current_iteration (int): the number of the current iteration
-        Returns:
-            if the current iteration should be reported, the iteration , else
-        """
-        if current_iteration%stage == 0:
-            print(f"Completed {current_iteration} out of {total} iterations")
-
-    return progress_test
-
-def voxel_into_5_tets(voxel_x, voxel_y, voxel_z, frac_to_cart, coord_store, connectivities_final):
-    """
-    convert a single voxel into 5 tets
-    Args:
-
-    """
-    coords =create_cube_coords(Coordinate(voxel_x, voxel_y, voxel_z), frac_to_cart)
-
-    indices = None
-    if is_odd(voxel_x, voxel_y, voxel_z):
-        indices = odd_cube_tet_indecies()
-    else:
-        indices = even_cube_tet_indices()
-
-        # test the tets and append those that pass
-    for tet in indices:
-        tet_indices = []
-        for index in tet:
-            tet_indices.append(coord_store.add(coords[index]))
-        connectivities_final.append(tet_indices)
-
-def voxels_to_5_tets_plain(mrc, threshold, progress):
-    """
-    converts voxels to tetrohedrons and returns those with average values above a threshold
-    Args:
-        mrc (mrcfile.mmap): the input file
-        threshold (float): the acceptance limit
-        progress (bool): if true print out progress
-    Returns:
-        (int): the number of voxels transformed
-        ([float, float, float] list): the coordinates of the vertices
-        ([int, int, int int] list): the vertices of the tets as indices in the coordinates list
-    """
-    coord_store = cu.UniqueTransformStore()
-    connectivities_final = []
-    voxel_count = count(0)
-    curent_iteration = count(1)
-    frac_to_cart = make_fractional_to_cartesian_conversion_function(mrc)
-    prog_test = make_progress_test(mrc.header.nx, mrc.header.ny, mrc.header.nz)
-
-    # Create an array of array of 8 point (co-ordinates) for each hexahedron (voxel)
-    for voxel_z in range(0, mrc.header.nz):
-        for voxel_y in range(0, mrc.header.ny):
-            for voxel_x in range(0, mrc.header.nx):
-                if progress:
-                    prog_test(next(curent_iteration))
-
-                # Threshold the voxels out of the mrc map data
-                if mrc.data[voxel_z, voxel_y, voxel_x] > threshold:
-                    voxel_into_5_tets(voxel_x, voxel_y, voxel_z, frac_to_cart, coord_store, connectivities_final)
-                    next(voxel_count)
-
-    points = [[coord.cart.x, coord.cart.y, coord.cart.z] for coord in coord_store.to_list()]
-
-    return next(voxel_count), points, connectivities_final
-
 def convert_mrc_to_5tets(input_file, output_file, threshold, ffea_out, vtk_out, verbose, progress):
     """
     Converts the contents of an mrc file to a tetrohedron array.
@@ -185,6 +98,101 @@ def convert_mrc_to_5tets_interp(input_file, output_file, threshold, ffea_out, vt
 
         write_tets_to_files(points, tet_connectivities, output_file, ffea_out, vtk_out)
 
+def make_progress_test(end_x, end_y, end_z, steps=10, start_x=0, start_y=0, start_z=0):
+    """
+    make a progress test function
+    Args:
+        end_x (int): number of x columns
+        end_y (int): number of y columns
+        end_z (int): number of z columns
+        steps (int): the number of iterations between reporting
+        start_x (int): the count (zero start) of the first x column
+        start_y (int): the count (zero start) of the first y column
+        start_z (int): the count (zero start) of the first z column
+    Returns:
+        function(int, int, int) =>int or None
+        (int): the total number of iterations
+    """
+    total = (end_x - start_x) * (end_y - start_y) * (end_z - start_z)
+    stage = int(total/steps)
+
+    def progress_test(current_iteration):
+        """
+        test if the current iteration should be reported for prograss
+        Args:
+            current_iteration (int): the number of the current iteration
+        Returns:
+            if the current iteration should be reported, the iteration , else
+        """
+        if current_iteration%stage == 0:
+            print(f"Completed {current_iteration} out of {total} iterations")
+
+    return progress_test
+
+def plain_voxel_to_5_tets(voxel, frac_to_cart, coord_store, connectivities):
+    """
+    convert a single voxel into 5 tets
+    Args:
+        voxel (Coordinage): array indices of voxel
+        frac_to_cart (function fractional => cartesian): coordinages transform
+        coord_store (CoordTransform, []): list for coordinates
+        connectivities ([[int, int, int int]]): tet connectivities
+    Returns:
+        None
+    """
+    coords =create_cube_coords(voxel, frac_to_cart)
+
+    indices = None
+    if is_odd(voxel.x, voxel.y, voxel.z):
+        indices = odd_cube_tet_indecies()
+    else:
+        indices = even_cube_tet_indices()
+
+        # test the tets and append those that pass
+    for tet in indices:
+        tet_indices = []
+        for index in tet:
+            tet_indices.append(coord_store.add(coords[index]))
+        connectivities.append(tet_indices)
+
+def voxels_to_5_tets_plain(mrc, threshold, progress):
+    """
+    converts voxels to tetrohedrons and returns those with average values above a threshold
+    Args:
+        mrc (mrcfile.mmap): the input file
+        threshold (float): the acceptance limit
+        progress (bool): if true print out progress
+    Returns:
+        (int): the number of voxels transformed
+        ([float, float, float] list): the coordinates of the vertices
+        ([int, int, int int] list): the vertices of the tets as indices in the coordinates list
+    """
+    coord_store = cu.UniqueTransformStore()
+    connectivities_final = []
+    voxel_count = count(0)
+    curent_iteration = count(1)
+    frac_to_cart = make_fractional_to_cartesian_conversion_function(mrc)
+    prog_test = make_progress_test(mrc.header.nx, mrc.header.ny, mrc.header.nz)
+
+    # Create an array of array of 8 point (co-ordinates) for each hexahedron (voxel)
+    for voxel_z in range(0, mrc.header.nz):
+        for voxel_y in range(0, mrc.header.ny):
+            for voxel_x in range(0, mrc.header.nx):
+                if progress:
+                    prog_test(next(curent_iteration))
+
+                # Threshold the voxels out of the mrc map data
+                if mrc.data[voxel_z, voxel_y, voxel_x] > threshold:
+                    plain_voxel_to_5_tets(Coordinate(voxel_x, voxel_y, voxel_z),
+                                          frac_to_cart,
+                                          coord_store,
+                                          connectivities_final)
+                    next(voxel_count)
+
+    points = [[coord.cart.x, coord.cart.y, coord.cart.z] for coord in coord_store.to_list()]
+
+    return next(voxel_count), points, connectivities_final
+
 def voxels_to_5_tets_interp(mrc, threshold, progress):
     """
     converts voxels to tetrohedrons and returns those with average values above a threshold
@@ -223,30 +231,50 @@ def voxels_to_5_tets_interp(mrc, threshold, progress):
                     # count the number of voxels
                     next(voxel_count)
 
-                    # make the matching coordinates
-                    coords = create_cube_coords(Coordinate(voxel_x, voxel_y, voxel_z), frac_to_cart)
-
-                    # connectivity of 5 tets in single voxel
-                    indices = None
-                    if is_odd(voxel_x, voxel_y, voxel_z):
-                        indices = odd_cube_tet_indecies()
-                    else:
-                        indices = even_cube_tet_indices()
-
-                    # test the tets and append those that pass
-                    for tet in indices:
-                        average = 0.0
-                        for index in tet:
-                            average += cube_vertex_values[index]
-                        average /= 4
-                        if average > threshold:
-                            tet_indices = []
-                            for index in tet:
-                                tet_indices.append(coord_store.add(coords[index]))
-                            connectivities_final.append(tet_indices)
+                    interp_voxel_to_5_tets(Coordinate(voxel_x, voxel_y, voxel_z),
+                                           frac_to_cart,
+                                           threshold,
+                                           cube_vertex_values,
+                                           coord_store,
+                                           connectivities_final)
 
     tmp = [[coord.cart.x, coord.cart.y, coord.cart.z] for coord in coord_store.to_list()]
     return next(voxel_count), tmp, connectivities_final
+
+def interp_voxel_to_5_tets(voxel, frac_to_cart, threshold, cube_vertex_values, coord_store, connectivities):
+    """
+    convert a voxel to 5 tets thresholding the tets by interpolating vertex values
+    Args:
+        voxel (Coordinate): the array indices of the voxel
+        frac_to_cart (fractional => cartesian): coordinates conversion function
+        threshold (float): threshold for inclusion
+        cube_vertex_values ([float]): the image values interpolated at voxel vertices
+        coord_store (UniqueTransformStore): store for the vertices
+        connectivities ([[int, int, int, int]]): the tet's connectivites
+    Returns:
+        None
+    """
+    # make the matching coordinates
+    coords = create_cube_coords(voxel, frac_to_cart)
+
+    # connectivity of 5 tets in single voxel
+    indices = None
+    if is_odd(voxel.x, voxel.y, voxel.z):
+        indices = odd_cube_tet_indecies()
+    else:
+        indices = even_cube_tet_indices()
+
+    # test the tets and append those that pass
+    for tet in indices:
+        average = 0.0
+        for index in tet:
+            average += cube_vertex_values[index]
+        average /= 4
+        if average > threshold:
+            tet_indices = []
+            for index in tet:
+                tet_indices.append(coord_store.add(coords[index]))
+            connectivities.append(tet_indices)
 
 def even_cube_tet_indices():
     """
