7676384c48eaa523b23a73ed949e57d3020a8853
Jonathan Pickering <J.H.Pickering@leeds.ac.uk>
2023-05-09 12:10:09 +0100

    demo working for mrc files
    Former-commit-id: 9cc899fffc8942faf68b78a8818be54b824776c3

--- view_section.py
+++ view_section.py
@@ -2,11 +2,12 @@ import argparse
 import pathlib
 import numpy as np
 import mrcfile
+import itertools
 from PIL import Image
 
 import ffeamesh.mrclininterpolate as mli
 
-def display_cross_section(image, xsize, ysize, z, file_name):
+def display_cross_section(image, xsize, zsize, y, isovalue, file_name):
     """
     display a section of the file
     Args:
@@ -14,27 +15,27 @@ def display_cross_section(image, xsize, ysize, z, file_name):
         args (argparse.namespace): options
     """
     x_range = np.linspace(image.offset_x, image.inner_size_x, xsize)
-    y_range = np.linspace(image.offset_y, image.inner_size_y, ysize)
+    z_range = np.linspace(image.offset_z, image.inner_size_z, zsize)
 
-    slice = np.zeros((len(x_range), len(y_range)))
+    slice = np.zeros((len(x_range), len(z_range)))
 
     for i, x in enumerate(x_range):
-        for j, y in enumerate(y_range):
+        for j, z in enumerate(z_range):
             slice[i, j] = image.density_at(x, y, z)
 
-    show_image(slice, file_name)
+    show_image(slice, isovalue, file_name)
 
-def show_image(slice_array, file_name):
+def show_image(slice_array, isovalue, file_name):
     image_array = np.zeros(slice_array.shape, dtype=np.uint8)
 
     for i in range(image_array.shape[0]):
         for j in range(image_array.shape[1]):
-            if slice_array[i, j] > 0.0001:
-                image_array[i, j] = 255.255
+            if slice_array[i, j] > isovalue:
+                image_array[i, j] = 255
 
     img = Image.fromarray(image_array, "L")
     # Display the Numpy array as Image
-    img.show()
+    #img.show()
     img.save(file_name)
 
 def get_args():
@@ -53,33 +54,38 @@ def get_args():
                         "--xsize",
                         type=int,
                         required=True,
-                        help="x coordinate")
-
-    parser.add_argument("-y",
-                        "--ysize",
-                        type=int,
-                        required=True,
-                        help="y coordinate")
+                        help="x steps")
 
     parser.add_argument("-z",
-                        "--zcoord",
+                        "--zsize",
+                        type=int,
+                        required=True,
+                        help="z steps")
+
+    parser.add_argument("-v",
+                        "--isovalue",
                         type=float,
                         required=True,
-                        help="z coordinate")
+                        help="value defining surface")
 
     return parser.parse_args()
 
 def main():
     """
-    run the demo
+    demo of linear interpolation in data from mrc file:
+    map_equilibrium_mystructrigor_15A_0p00202.mrc
     """
     args = get_args()
-    with mrcfile.open(args.input, mode='r+') as mrc:
+    #with mrcfile.open(args.input, mode='r+') as mrc:
+    with mrcfile.mrcmemmap.MrcMemmap(args.input) as mrc:
         image = mli.MRCImage(mrc)
-        for i in range(10, 90, 10):
-            z = float(i)
-            file_name = f"zcord_{i}.png"
-            display_cross_section(image, args.xsize, args.ysize, z, file_name)
+        count = itertools.count(1)
+        total = 305-45
+        for i in range(45, 305, 1):
+            y = float(i)
+            file_name = f"yscan_{i}.png"
+            display_cross_section(image, args.xsize, args.zsize, y, args.isovalue, file_name)
+            print(f"Image {next(count)} of {total}")
 
 if __name__ == "__main__":
     main()
