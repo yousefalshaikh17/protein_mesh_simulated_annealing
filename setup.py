fff7ed9fff07b2bdd5b64b1d9054c9c8152b2929
Jonathan Pickering <J.H.Pickering@leeds.ac.uk>
2023-11-22 13:45:56 +0000

    updated setup
    Former-commit-id: 10dcddfcd8ca59464a3a72737fef44666b055a45

--- setup.py
+++ setup.py
@@ -48,19 +48,14 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'fft_smooth = ffeamesh.scripts.fft_smooth:main',        
-            'five_tets = ffeamesh.scripts.five_tets:main',
             'make_test_mrc_file = ffeamesh.scripts.make_test_mrcfile:main',
             'make_tet_mesh_examples = ffeamesh.scripts.make_tet_mesh_examples:main',
+            'mrc_coarsen = ffeamesh.scripts.mrc_coarsen:main',
             'mrc_crop = ffeamesh.scripts.mrc_crop:main',
-            'mrc_density_section = ffeamesh.scripts.mrc_density_section:main',
             'mrc_header_info = ffeamesh.scripts.mrc_header_info:main',
             'mrc_image_stats = ffeamesh.scripts.mrc_image_stats:main',
             'mrc_threshold = ffeamesh.scripts.mrc_threshold:main',
             'mrc_voxel_size = ffeamesh.scripts.mrc_voxel_size:main',
-            'sim_anneal = ffeamesh.scripts.sim_anneal:main',            
-            'six_tets = ffeamesh.scripts.six_tets:main',
-            'zoom = ffeamesh.scripts.zoom:main',
             'tgv = ffeamesh.app_tgv.tgv:main'
         ]
     }
